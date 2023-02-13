37fe4c75434398db4779d79567126356331678aa
Ivan <pifiko@ukr.net>
2023-02-14 02:09:09 +0300

    fin

--- components/NewsBlock/NewsBlock.js
+++ components/NewsBlock/NewsBlock.js
@@ -1,10 +1,9 @@
 import style from './NewsBlock.module.scss';
 
 
-
 const NewsBlock = ({state}) => {
-    const {data ,name} = state
-
+    const {colNews ,name,News} = state
+    console.log(News[0]);
     return(
         <div className={style.politics}>
         <div className={style.wrapperCol}>
@@ -12,64 +11,40 @@ const NewsBlock = ({state}) => {
             <button className={style.allCol}>Всі новини розділу</button>
         </div>
         <div className={style.wrapperPolNews}>
-            <div className={style.bigNews}>
-                <img src="/image.jpg" alt="image" />
-                <div className={style.timeBig}>05 серпня 11:00</div>
-                <div className={style.bigTitle}>У "Слузі" пояснили, чому в росії знову заговорили про переговори з Україною.</div>
+            {News[0].map(({title, time, photo, id}) =>{
+                return(
+                    <div className={style.bigNews} key = {id}>
+                        <img src={photo} alt="image" />
+                        <div className={style.timeBig}>{time}</div>
+                        <div className={style.bigTitle}>{title}</div>
+                    </div>
+                )
+            })}
+            <div className={style.litNews} >
+            {News[1].map(({title, time, photo, id}) =>{
+                return(
+                    <div className={style.litNewsCard} key = {id}>
+                        <img src={photo} alt="image" />
+                        <div className={style.timeBig}>{time}</div>
+                        <div className={style.litTitle}>{}{title}</div>
+                    </div>
+                )
+            })}
             </div>
-            <div className={style.litNews}>
-                <div className={style.litNewsCard}>
-                    <img src="/image.jpg" alt="image" />
-                    <div className={style.timeBig}>05 серпня 11:00</div>
-                    <div className={style.litTitle}>У "Слузі" пояснили, чому в росії знову заговорили про переговори з Україною.</div>
-                </div>
-                <div className={style.litNewsCard}>
-                    <img src="/image.jpg" alt="image" />
-                    <div className={style.timeBig}>05 серпня 11:00</div>
-                    <div className={style.litTitle}>У "Слузі" пояснили, чому в росії знову заговорили про переговори з Україною.</div>
-                </div>
-            </div>
+            
             <div className={style.colNews}>
             <div className={style.colNewsTitle}>Новини розділу</div>
-                {data.map(({title, time, filter, id}) =>{
-                    if(filter == 'fire'){
+                {colNews.map(({title, time, filter, id}) =>{
                     return(
                         <div key={id} className = {style.newsInfo}>
-                            <a href="" className = {style.newsHeve}>
-                                <img src='/svg/fire.svg' alt="" />
-                                <div className= {style.newsHeveDesFire}>Термінова новина</div>
-                            </a>
+                            {filter === 'video' ? <a href="" className = {style.newsHeve}><img src='/svg/Exclude.svg' alt="video" /><span className= {style.newsHeveDesCam}>Новина з відео</span></a> : null}
+                            {filter === 'fire' ? <a href="" className = {style.newsHeve}><img src='/svg/fire.svg' alt="fire" /><span className= {style.newsHeveDesFire}>Термінова новина</span></a> : null}
                             <a href="" className = {style.newsDes}>
                                 <span className = {style.timer}>{time}</span> {title}
                             </a>
                             <hr />  
                         </div> 
-                    )    
-                    }if(filter == 'video'){
-                        return(
-                            <div key={id} className = {style.newsInfo}>
-                                <a href="" className = {style.newsHeve}>
-                                    <img src='/svg/Exclude.svg' alt="" />
-                                    <div className= {style.newsHeveDesCam}>Новина з відео</div>
-                                </a>
-                                <a href="" className = {style.newsDes}>
-                                    <span className = {style.timer}>{time}</span> {title}
-                                </a>
-                                <hr />  
-                            </div> 
-                        )    
-                        }
-                    if(filter == null){
-                        return(
-                            <div key={id} className = {style.newsInfo}>
-                                <a href="" className = {style.newsDes}>
-                                    <span className = {style.timer}>{time}</span> {title}
-                                </a>
-                                <hr />  
-                            </div> 
-                        )
-                        
-                    }
+                    )   
                 })}
                 <button className = {style.allNews}>Більше <div className={style.ring}><img src="/svg/arrowright.svg" alt="arrow" /></div></button>  
             </div>
