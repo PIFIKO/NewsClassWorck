8a947a6ad8d948617dba550c696f19550af53aa4
Your Name <you@example.com>
2023-02-13 03:26:27 +0300

    fin

--- pages/politics/index.js
+++ pages/politics/index.js
@@ -3,18 +3,28 @@ import Writers from '../../components/Writers/Writers'
 import NewsBlock from '../../components/NewsBlock/NewsBlock';
 import PromoBlock from '../../components/PromoBlock/PromoBlock';
 
+const state  = {
+  data:[
+  { id: 1, title: 'До Європи за захистом. Що означає позов Ахметова до Європейського суду з прав людини' , time: '14:59', filter: 'fire'},
+  { id: 2, title: 'До Європи за захистом. Що означає позов Ахметова до Європейського суду з прав людини' , time: '14:59', filter: null},
+  { id: 3, title: '"Відбудова України — завдання усього світу": промова Зеленського на конференції в Лугано' , time: '14:59', filter: 'video'},
+  { id: 4, title: 'Бийся на світлій стороні": як спортсмени, меценати й волонетри рятують життя українців' , time: '14:59', filter: null},
+  { id: 5, title: 'Бийся на світлій стороні": як спортсмени, меценати й волонетри рятують життя українців' , time: '14:59', filter: null},
+  
+  ],
+  name: 'Політика'
+};
 
 
 
-
-const Politics = () => {
+const Politics = () => {  
 
   return(
     <div className={style.container}>
       <div className={style.wrapperPol}>
         <Writers/>
         <div>
-          <NewsBlock/>
+          <NewsBlock  state ={state}/>
           <PromoBlock/>
         </div>
       </div>
